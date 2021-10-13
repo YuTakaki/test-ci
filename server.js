8b03c4ca36e8ead0497adb2ae6b6f117789d79b5
yutakaki <yu.takaki2000@gmail.com>
2021-10-13 12:04:09 +0900

    change

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('asdasd')
+    res.send('Hello first deploy')
 })
 
 app.listen(process.env.PORT || 3000, () => {
